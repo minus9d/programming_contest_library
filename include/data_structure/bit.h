ed33b89c9c76e39df52470cd295459f4734ec958
minus9d <minus9d@gmail.com>
2017-10-15 10:45:27 +0900

    bitを追加

---
+++ include/data_structure/bit.h
@@ -0,0 +1,37 @@
+// BITの実装　数列の、最初の要素からある要素までの和を返す
+// クラスとして実装
+// 参考：プログラミングコンテストチャレンジブック 第一版 p161
+// verify: http://arc075.contest.atcoder.jp/tasks/arc075_c
+
+#pragma once
+#include "common.h"
+
+// 1 originなので注意!!
+class Bit
+{
+private:
+    int m_n;
+    vector<int> m_bit;
+    
+public:
+    void init(int n) {
+        m_n = n;
+        m_bit.resize(n + 1);
+    }
+
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += m_bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+    
+    void add(int i, int x) {
+        while (i <= m_n) {
+            m_bit[i] += x;
+            i += i & -i;
+        }
+    }
+};
---
+++ test/data_structure/bit.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+#include "data_structure/bit.h"
+
+TEST_CASE("bit", "[data_structure]") {
+    Bit bit;
+    int N = 8;
+    bit.init(N);
+    for(int i = 0; i < N; ++i) {
+        bit.add(i + 1, i + 1);
+    }
+    REQUIRE(bit.sum(1) == 1);
+    REQUIRE(bit.sum(2) == 3);
+    REQUIRE(bit.sum(3) == 6);
+    REQUIRE(bit.sum(4) == 10);
+    REQUIRE(bit.sum(5) == 15);
+    REQUIRE(bit.sum(6) == 21);
+    REQUIRE(bit.sum(7) == 28);
+    REQUIRE(bit.sum(8) == 36);
+}
