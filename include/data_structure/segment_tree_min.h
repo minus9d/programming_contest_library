78b1a75b1d260fa5204bccaa5c2176cb0bbffcb5
minus9d <minus9d@gmail.com>
2017-12-19 22:40:14 +0900

    segment treeで最小値の位置を出す関数を追加

--- include/data_structure/segment_tree_min.h
+++ include/data_structure/segment_tree_min.h
@@ -38,3 +38,48 @@ private:
         }
     }
 };
+
+// (最小値, 最小値をとるインデックス)のペアを返す
+// 複数の値が同時に最小値を取る場合はどちらのインデックスが返るか保証がないので注意
+// 戻る値など要確認！！！
+template <typename T>
+class SegmentTreeMinWithIndex
+{
+private:
+    T INF;
+    int m_n;
+    vector<pair<T,int>> m_dat; // (min, idx)のペア
+public:
+    SegmentTreeMinWithIndex(int n)
+        : INF(std::numeric_limits<T>::max()) {
+        m_n = 1;
+        while  (m_n < n) m_n *= 2;
+        m_dat.resize(2 * m_n - 1, mp(INF,-1));
+    }
+    void update(int k, T val) {
+        int orig_k = k;
+        k += m_n - 1;
+        m_dat[k] = mp(val,orig_k);
+        while (k > 0) {
+            k = (k - 1) / 2;
+            auto v1 = m_dat[k * 2 + 1].first;
+            auto v2 = m_dat[k * 2 + 2].first;
+            if (v1 < v2) m_dat[k] = mp(v1, m_dat[k * 2 + 1].second);
+            else m_dat[k] = mp(v2, m_dat[k * 2 + 2].second);
+        }
+    }
+    // [a, b)の範囲で(min, idx)のペアを返す
+    pair<T,int> query(int a, int b) {
+        return queryInternal(a, b, 0, 0, m_n);
+    }
+private:
+    pair<T,int> queryInternal(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) return mp(INF,-1);
+        if (a <= l && r <= b) return m_dat[k]; // ?
+        else {
+            auto vl = queryInternal(a, b, k * 2 + 1, l, (l + r) / 2);
+            auto vr = queryInternal(a, b, k * 2 + 2, (l + r) / 2, r);
+            return (vl.first < vr.first) ? vl : vr;
+        }
+    }
+};
--- test/data_structure/segment_trees.cpp
+++ test/data_structure/segment_trees.cpp
@@ -25,3 +25,17 @@ TEST_CASE("segment_tree_min", "[data_structure]") {
     REQUIRE(st.query(0, 8) == -3);
     REQUIRE(st.query(0, 4) == 1);
 }
+
+TEST_CASE("segment_tree_min_with_index", "[data_structure]") {
+    SegmentTreeMinWithIndex<int> st(8);
+    st.update(0, 5);
+    st.update(1, 2);
+    st.update(2, 7);
+    st.update(3, 1);
+    st.update(4, -3);
+    st.update(5, 4);
+    st.update(6, 8);
+    st.update(7, 2);
+    REQUIRE(st.query(0, 8) == mp(-3, 4));
+    REQUIRE(st.query(0, 4) == mp(1, 3));
+}
