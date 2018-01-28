ad26a46a8a16847483df911dfa103efb0ad9d2ff
minus9d <minus9d@gmail.com>
2018-01-28 12:32:15 +0900

    最大流を用いたbipartite-matchingを追加、初期化の方式を合わせる

--- include/graph/bipartite-matching.h
+++ include/graph/bipartite-matching.h
@@ -1,24 +1,29 @@
+// 二部マッチングを2種類の方法で解く
+// 参考：プログラミングコンテストチャレンジブック 第一版 p196, p197
+
 #pragma once
 #include "common.h"
+#include "max-flow-ford-fulkerson.h"
 
 class BipartiteMatching {
 public:
-    // V: 両側の頂点数の和
-    BipartiteMatching(int V) : m_V(V) {
-        m_G.resize(m_V);
+    // V1, V2: 左側・右側それぞれの頂点数
+    BipartiteMatching(int V1, int V2) : m_V1(V1), m_V2(V2) {
+        m_G.resize(m_V1 + m_V2);
     }
-    // Uの頂点とVの頂点の間に辺を張る (u: 左側  v: 右側)
-    void add_edge(int u, int v) {
-        m_G[u].push_back(v);
-        m_G[v].push_back(u);
+    // 左側の頂点と右側の頂点の間に辺を張る
+    void add_edge(int v1, int v2) {
+        v2 += m_V1;
+        m_G[v1].push_back(v2);
+        m_G[v2].push_back(v1);
     }
     // 二部グラフの最大マッチングを求める
     int bipartite_matching() {
         int res = 0;
-        m_match.resize(m_V, -1);
-        REP(v, m_V) {
+        m_match.resize(m_V1 + m_V2, -1);
+        REP(v, m_V1 + m_V2) {
             if (m_match[v] < 0) {
-                m_used.resize(m_V, 0);
+                m_used.resize(m_V1 + m_V2, 0);
                 if (dfs(v)) ++res;
             }
         }
@@ -46,8 +51,38 @@ private:
         }
         return false;
     }
-    int m_V; // 両側の頂点数の合計
+    int m_V1;
+    int m_V2;
     vector<vector<int>> m_G; // グラフの隣接リスト表現
     vector<int> m_match; // マッチングのペア
     vector<char> m_used; // DFSですでに調べたかのフラグ
+};
+
+
+// 通常使う必要なし
+class BipartiteMatchingFF {
+public:
+    // それぞれの部の頂点の数を指定
+    BipartiteMatchingFF(int in_V1, int in_V2)
+        : m_V1(in_V1), m_V2(in_V2) {
+        // 始点、終点を加えるので+2
+        m_ff = new FordFulkerson<int>(m_V1 + m_V2 + 2);
+        s = m_V1 + m_V2;
+        t = s + 1;
+        // 始点から辺を張る
+        REP(i, m_V1) m_ff->add_uni_edge(s, i, 1);
+        // 終点へ辺を張る
+        REP(i, m_V2) m_ff->add_uni_edge(m_V1 + i, t, 1);
+    }
+    ~BipartiteMatchingFF(void) { delete m_ff; }
+    // V1の頂点とV2の頂点の間に辺を張る
+    void add_edge(int v1, int v2) { m_ff->add_uni_edge(v1, m_V1 + v2, 1); }
+    // 二部グラフの最大マッチングを求める
+    int bipartite_matching() { return m_ff->max_flow(s, t); }
+private:
+    int m_V1; // 始点側の部の要素数
+    int m_V2; // 終点側の部の要素数
+    int s; // 始点
+    int t; // 終点
+    FordFulkerson<int>* m_ff;
 };
--- test/graph/bipartite-matching.cpp
+++ test/graph/bipartite-matching.cpp
@@ -7,14 +7,30 @@ TEST_CASE("bipartite-matching", "[graph]") {
     // 人の数
     const int K = 6;
 
-    BipartiteMatching bm(N + K);
+    BipartiteMatching bm(N, K);
 
-    bm.add_edge(0, 0 + N);
-    bm.add_edge(0, 1 + N);
-    bm.add_edge(0, 2 + N);
-    bm.add_edge(1, 3 + N);
-    bm.add_edge(2, 1 + N);
-    bm.add_edge(3, 4 + N);
+    bm.add_edge(0, 0);
+    bm.add_edge(0, 1);
+    bm.add_edge(0, 2);
+    bm.add_edge(1, 3);
+    bm.add_edge(2, 1);
+    bm.add_edge(3, 4);
+
+    int max_pair = bm.bipartite_matching();
+    REQUIRE(max_pair == 4);
+}
+
+TEST_CASE("bipartite-matching-ff", "[graph]") {
+    const int N = 4;
+    const int K = 6;
+    BipartiteMatchingFF bm(N, K);
+
+    bm.add_edge(0, 0);
+    bm.add_edge(0, 1);
+    bm.add_edge(0, 2);
+    bm.add_edge(1, 3);
+    bm.add_edge(2, 1);
+    bm.add_edge(3, 4);
 
     int max_pair = bm.bipartite_matching();
     REQUIRE(max_pair == 4);
