9f5006f0aa40014e3f6b87eb7078b5acd2ecd7b8
minus9d <minus9d@gmail.com>
2019-02-26 23:32:19 +0900

    和が0以上の部分配列の数を数える関数を追加

--- src/data_structure/bit_app.cpp
+++ src/data_structure/bit_app.cpp
@@ -6,3 +6,9 @@ TEST_CASE("bubbleSortSwapNum", "[data_structure]") {
     REQUIRE(bubbleSortSwapNum<int>(vector<int>{1, 2, 3, 4}) == 0);
     REQUIRE(bubbleSortSwapNum<int>(vector<int>{4, 3, 2, 1}) == 6);
 }
+
+TEST_CASE("getNumberOfSegmentsWhoseSumIsNotNegative", "[data_structure]") {
+    REQUIRE(getNumberOfSegmentsWhoseSumIsNotNegative<int>(vector<int>{-1, 1, -1}) == 3);
+    REQUIRE(getNumberOfSegmentsWhoseSumIsNotNegative<int>(vector<int>{1, 1, 1}) == 6);
+    REQUIRE(getNumberOfSegmentsWhoseSumIsNotNegative<int>(vector<int>{-1, -1, -1}) == 0);
+}
--- src/data_structure/bit_app.h
+++ src/data_structure/bit_app.h
@@ -4,12 +4,51 @@
 
 // bit 応用
 
+// Q. 長さNの配列がある。連続する部分配列N(N+1)/2通りのうち、
+//    その和が0以上であるものの個数を求めよ
+// 例: 配列が[-1, 1, -1] の場合, 3個
+// 参考: https://atcoder.jp/contests/arc101/tasks/arc101_b
+//      区間[l, r]の和が0以上であることと、S_r - S_{l-1} >= 0 は同値なのを利用
+//      後者は転倒数と同様に、累積和をうまく使って計算可能。
+//      BITを使うためには全要素が1以上であることが必要なため下駄を履かせる
+//
+//      例えば配列が[-1, 1, -1]の場合
+//      累積和は    [-1, 0, -1]。
+//      頭に0を入れると計算しやすいので入れて[0, -1, 0, -1]
+//      ここで 全体に+2だけ下駄を履かせて [2, 1, 2, 1]
+//      あとは、2番目の要素から順に、「自分より左にある要素のうち
+//      自分以下の要素の数」を足せばよい
+
+template <typename T>
+ll getNumberOfSegmentsWhoseSumIsNotNegative(const vector<T>& arr) {
+    auto N = SIZE(arr);
+    // まず累積和作成  のちの計算を簡単にするため先頭に0を入れる
+    vector<T> cum(N+1);
+    REP(i, N) cum[i + 1] += cum[i] + arr[i];
+
+    // BITを使うため最小値を1に
+    auto mn = *min_element(ALL(cum));
+    auto offset = -mn + 1;
+    for(auto& e: cum) e += offset;
+    auto mx = *max_element(ALL(cum));
+
+    ll ans = 0;
+    Bit bit; bit.init(mx);
+    bit.add(offset, 1);
+    FOR(i, 1, N + 1) {
+        auto c = cum[i];
+        ans += bit.sum(c);
+        bit.add(c, 1);
+    }
+    return ans;
+}
+
 // Q. 1..Nを並び替えた長さNの配列がある。転倒数(=バブルソートの交換回数)を求めよ。
 // 例: [3, 1, 4, 2]の転倒数は3
 // 参考：プログラミングコンテストチャレンジブック 第一版 p162
 // 注：値が飛び飛びの場合は事前に座標圧縮が必要
 template <typename T>
-ll bubbleSortSwapNum(const vector<int>& arr) {
+ll bubbleSortSwapNum(const vector<T>& arr) {
     ll ans = 0;
     ll N = SIZE(arr);
     Bit bit; bit.init(N);
