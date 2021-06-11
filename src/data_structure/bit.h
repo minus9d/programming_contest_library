578cd7b978090819dd7e660b6ebb41326eb6d765
minus9d <minus9d@gmail.com>
2021-06-11 21:38:55 +0900

    bitで外から呼ばない関数をprivate化 / コメント付与

--- src/data_structure/bit.h
+++ src/data_structure/bit.h
@@ -6,8 +6,7 @@
 #pragma once
 #include "common.h"
 
-// 区間加算に対応しないBIT
-// 1 originなので注意!!
+// 区間加算に対応しないBIT (1-based)
 class BitSimple
 {
 private:
@@ -39,7 +38,7 @@ public:
 };
 
 // https://github.com/drken1215/algorithm/blob/master/DataStructure/binary_indexed_tree.cpp
-// 区間加算にも対応した BIT
+// 区間加算にも対応した BIT (1-based)
 // 参考: https://www.slideshare.net/hcpc_hokudai/binary-indexed-tree
 template <class Abel> struct BIT {
     vector<Abel> dat[2];
@@ -51,28 +50,31 @@ template <class Abel> struct BIT {
         REP(iter, 2) dat[iter].assign(n + 1, UNITY_SUM);
     }
     
-    /* a, b are 1-indexed, [a, b) */
-    inline void sub_add(int p, int a, Abel x) {
-        for (int i = a; i < (int)dat[p].size(); i += i & -i)
-            dat[p][i] = dat[p][i] + x;
-    }
+    // 区間[a, b)に値xを加算
     inline void add(int a, int b, Abel x) {
         sub_add(0, a, x * -(a - 1)); sub_add(1, a, x); sub_add(0, b, x * (b - 1)); sub_add(1, b, x * (-1));
     }
-    
-    /* a is 1-indexed, [a, b) */
-    inline Abel sub_sum(int p, int a) {
-        Abel res = UNITY_SUM;
-        for (int i = a; i > 0; i -= i & -i) res = res + dat[p][i];
-        return res;
-    }
+
+    // 区間[a, b)の和を返す
     inline Abel sum(int a, int b) {
         return sub_sum(0, b - 1) + sub_sum(1, b - 1) * (b - 1) - sub_sum(0, a - 1) - sub_sum(1, a - 1) * (a - 1);
     }
-    
+
     /* debug */
     void print() {
         for (int i = 1; i < (int)dat[0].size(); ++i) cout << sum(i, i + 1) << ",";
         cout << endl;
+    }
+
+private:
+    inline void sub_add(int p, int a, Abel x) {
+        for (int i = a; i < (int)dat[p].size(); i += i & -i)
+            dat[p][i] = dat[p][i] + x;
+    }
+
+    inline Abel sub_sum(int p, int a) {
+        Abel res = UNITY_SUM;
+        for (int i = a; i > 0; i -= i & -i) res = res + dat[p][i];
+        return res;
     }
 };
